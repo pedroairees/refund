4fe143cbbb1a86eab490faee631defe985e55557
pedroairees <pedrocoelho2209@gmail.com>
2025-10-20 18:31:39 -0300

    funcao para formatar moeda BRL

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ const inputAmount = document.getElementById('amount')
 
 inputAmount.oninput = () => {
     let value = inputAmount.value.replace(/\D/g, "")
+    value = Number(value) / 100
+    inputAmount.value = formatCurrencyBRL(value)
+}
 
-    inputAmount.value = value
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return value
 }
