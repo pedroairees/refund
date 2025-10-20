dae69ecdb94c989db94ed7ccbc728812baa2148b
pedroairees <pedrocoelho2209@gmail.com>
2025-10-20 20:53:31 -0300

    capturando os valores

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-const inputAmount = document.getElementById('amount')
+const form = document.querySelector('form')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
+const amount = document.getElementById('amount')
 
-inputAmount.oninput = () => {
-    let value = inputAmount.value.replace(/\D/g, "")
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
     value = Number(value) / 100
-    inputAmount.value = formatCurrencyBRL(value)
+    amount.value = formatCurrencyBRL(value)
 }
 
 function formatCurrencyBRL(value) {
@@ -12,4 +15,19 @@ function formatCurrencyBRL(value) {
         currency: 'BRL'
     })
     return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        crated_at: new Date(),
+    }
+
+    console.log(newExpense)
 }
